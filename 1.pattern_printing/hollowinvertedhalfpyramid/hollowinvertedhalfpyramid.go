bfa6863b14fe60f4f4ad759cb0bdf1fbcda245bb
hellosumitg <hellosumitg.com>
2023-11-13 19:05:41 +0530

    added hollowinvertedhalfpyramid file and modified hollowrectangle and invertedhalfpyramid files

--- 1.pattern_printing/hollowinvertedhalfpyramid/hollowinvertedhalfpyramid.go
+++ 1.pattern_printing/hollowinvertedhalfpyramid/hollowinvertedhalfpyramid.go
@@ -3,19 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var rows, cols int
-
+	var rows int
 	fmt.Print("Enter the number of rows: ")
 	fmt.Scan(&rows)
 
-	fmt.Print("Enter the number of columns: ")
-	fmt.Scan(&cols)
-
-	// Loop through each row and column
-	for i := 1; i <= rows; i++ {
-		for j := 1; j <= cols; j++ {
+	for i := rows; i >= 1; i-- {
+		for j := 1; j <= i; j++ {
 			// If it's the first or last row or the first or last column, print an asterisk
-			if i == 1 || i == rows || j == 1 || j == cols {
+			if j == 1 || j == i || i == rows {
 				fmt.Print("* ")
 			} else {
 				fmt.Print("  ")
--- 1.pattern_printing/invertedhalfpyramid/invertedhalfpyramid.go
+++ 1.pattern_printing/invertedhalfpyramid/invertedhalfpyramid.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var rows int
+
+	fmt.Print("Enter the number of rows: ")
+	fmt.Scan(&rows)
+
+	// Outer loop to iterate through each row in reverse order (from bottom to top)
+	for i := rows; i >= 1; i-- {
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
+		}
+	fmt.Println()
+	}
 }
